abda3ee16e026e2c4c082cc2652e960ac27c4be2
KatyPreston <prestonkj@hotmail.com>
2018-05-29 15:38:25 +0100

    extensions completed

--- app/src/main/java/com/example/user/rockpaperscissors/RockPaperScissorGameActivity.java
+++ app/src/main/java/com/example/user/rockpaperscissors/RockPaperScissorGameActivity.java
@@ -29,32 +29,32 @@ public class RockPaperScissorGameActivity extends AppCompatActivity {
         game = new Game();
     }
 
-    public void onRockButtonClicked(View button){
+    public void onRockButtonClicked(View view){
         playerChoice.setText("Player chooses ROCK");
         winner.setText(game.play(Choice.ROCK));
         computerChoice.setText("Computer chooses " + game.result.toString());
     }
 
 
-    public void onPaperButtonClicked(View button){
+    public void onPaperButtonClicked(View view){
         playerChoice.setText("Player chooses PAPER");
         winner.setText(game.play(Choice.PAPER));
         computerChoice.setText("Computer chooses " + game.result.toString());
     }
 
-    public void onScissorsButtonClicked(View button){
+    public void onScissorsButtonClicked(View view){
         playerChoice.setText("Player chooses SCISSORS");
         winner.setText(game.play(Choice.SCISSORS));
         computerChoice.setText("Computer chooses " + game.result.toString());
     }
 
-    public void onLizardButtonClicked(View button){
+    public void onLizardButtonClicked(View view){
         playerChoice.setText("Player chooses LIZARD");
         winner.setText(game.play(Choice.LIZARD).toString());
         computerChoice.setText("Computer chooses " + game.result.toString());
     }
 
-    public void onSpockButtonClicked(View button){
+    public void onSpockButtonClicked(View view){
         playerChoice.setText("Player chooses SPOCK");
         winner.setText(game.play(Choice.SPOCK).toString());
         computerChoice.setText("Computer chooses " + game.result.toString());
